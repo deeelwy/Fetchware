0ba39ea2a55f14d9ba779f4c1348c2d8efc04d62
David Yingling <deeelwy@gmail.com>
2012-10-14 00:34:05 -0400

    Implemented and tested App::Fetchware::HTMLPageSync.
    This is the first extension for fetchware. It uses fetchware's stupidly
    simple API to "replace" App::Fetchware's behavior for its own.
    
    Next, I'm going to fix dzil support, and add all of my plugins back.

---
+++ lib/App/Fetchware/HTMLPageSync.pm
@@ -0,0 +1,434 @@
+###BUGALERT### Need msg() and vmsg()s!!!
+package App::Fetchware::HTMLPageSync;
+use strict;
+use warnings;
+
+# Enable Perl 6 knockoffs.
+use 5.010;
+
+# Load only the fetchware API subroutines that A::P::HTMLPageSync does not
+# override by implementing them here in its own subroutine.
+use App::Fetchware qw(start end temp_dir mirror config make_config_sub
+    do_nothing
+    :UTIL);
+
+# Local imports.
+use File::Copy 'cp';
+use File::Path 'remove_tree';
+
+# Set up Exporter to bring App::Fetchware's API to everyone who use's it
+# including fetchware's ability to let you rip into its guts, and customize it
+# as you need.
+use Exporter qw( import );
+
+# Export HTMLPageSync's version of App::Fetchware's API. This is what does the
+# "overriding," when a user puts use App::Fetchware::HTMLPageSync in their
+# Fetchwarefile, which will load this module, and import HTMLPageSync's API,
+# which implements and replaces (overrides) App::Fetchware's API.
+our @EXPORT = qw(
+    start
+    lookup
+    download
+    verify
+    unarchive
+    build
+    install
+    end
+    uninstall
+
+    page_name
+    html_page_url
+    destination_directory
+    user_agent
+    html_treebuilder_callback
+    download_links_callback
+);
+
+
+# App::Fetchware "subclasses" must forward declare any App::Fetchware api
+# subroutines that they will "override" just like App::Fetchware does.
+sub lookup (;$);
+sub download ($;$);
+sub verify ($;$);
+sub unarchive ($);
+sub build ($);
+sub install (;$);
+sub uninstall ($);
+
+
+# Since configuration subs have prototypes, and prototypes must be known at
+# compile time in order to be honored, I must wrap my calls to make_config_sub()
+# inside a BEGIN block so they happen at compile time.
+BEGIN {
+    # API functions using make_config_sub() can be ONE, ONEARRREF, MANY, or
+    # BOOLEAN. See (###BUGALERT### link to docs in whereever they are.
+    my @api_functions = (
+        [ page_name => 'ONE' ],
+        [ html_page_url => 'ONE' ],
+        [ destination_directory => 'ONE' ],
+        [ user_agent => 'ONE' ],
+        # return true for tags you want to sort through.
+        [ html_treebuilder_callback => 'ONE' ],
+        # get @download_urls, filter them, and return them again.
+        [ download_links_callback => 'ONE' ],
+    );
+
+    for my $api_function (@api_functions) {
+        make_config_sub(@{$api_function});
+    }
+}
+
+=head1 MOTIVATION
+
+I want to automatically parse a Web page with links to wall papers that I want
+to download. Only I want software to do it for me. That's where this
+App::Fetchware "subclass" comes in.
+
+=cut
+
+
+=head1 App::Fetchware::HTMLPageSync API SUBROUTINES
+
+This is App::Fetchware::HTMLPageSync's API that fetchware uses to execute any
+Fetchwarefile's that make use of App::Fetchware::HTMLPageSync. This API is the
+same that regular old App::Fetchware uses for most standard FOSS software.
+
+
+=item my $temp_file = start();
+
+start() creats a temp dir, chmod 700's it, and chdir()'s to it just like the one
+in App::Fetchware does. App::Fetchware::HTMLPageSync
+
+=cut
+
+
+=item my $download_url = lookup();
+
+lookup() downloads the user specified C<html_page_url>, parses it using
+HTML::TreeBuilder, and uses various yet to be written config subs if specified
+to maniuplate the tree to determine what download urls the user wants.
+
+This list of download urls is returned as an array reference, $download_url.
+
+=cut
+
+sub lookup (;$) {
+    msg
+    "Looking up download urls using html_page_url [@{[config('html_page_url')]}]";
+    ###BUGALERT### Create a user changeable version of lookup_check_args??(), so
+    #that App::Fetchware 'subclasses' can use it.
+    # Download the url the user specified.
+    my $filename = do {
+        if (defined config('user_agent')) {
+            download_http_url(config('html_page_url'),
+                user_agent =>  config('user_agent'));
+        } else {
+            download_http_url(config('html_page_url'));
+        }
+    };
+    vmsg "Downloaded html_page_url to local file [$filename].";
+
+    # Create a HTML::TreeBuilder object for the now downloaded file.
+    my $tree = HTML::TreeBuilder->new();
+    # Parse $filename into a HTML::Element tree.
+    $tree->parse_file($filename);
+    vmsg 'Created HTML::TreeBuilder object to parse downloaded html file.';
+
+    my $tree_callback = do {
+        if (config('html_treebuilder_callback')) {
+            vmsg <<EOM;
+Using user supplied html_treebuilder_callback to parse downloaded HTML file:
+[
+@{[config('html_treebuilder_callback')]}
+]
+EOM
+            config('html_treebuilder_callback');
+        } else {
+            vmsg <<EOM;
+Using built-in default html_treebuilder_callback that only wants images.
+EOM
+            sub {
+                my $tag = shift;
+                my $link = $tag->attr('href');
+                if (defined $link) {
+                    # If the anchor tag is an image...
+                    if ($link =~ /\.(jpg|jpeg|png|bmp|tiff?|gif)$/) {
+                        # ...return true...
+                        return 'True';
+                    } else {
+                        # ...if not return false.
+                        return undef; #false
+                    }
+                }
+            };
+        }
+    };
+
+    # Find the links that match our default callback or the user specified one
+    # if the user specified one.
+    my @download_urls = $tree->look_down(
+        _tag => 'a',
+        $tree_callback
+    );
+    vmsg <<EOM;
+Determined download urls to be:
+@download_urls
+EOM
+
+    # Sort through the list of HTML::Element tags to finalize the list to
+    # download.
+    my $links_callback = do {
+        if (config('download_links_callback')) {
+            vmsg <<EOM;
+Determined download_links_callback to be user specified:
+[
+@{[config('download_links_callback')]}
+]
+EOM
+            config('download_links_callback');
+        } else {
+            # Strip off HTML::Element crap by default.
+            sub {
+                vmsg <<EOM;
+Using built-in default download_links_callback that turns HTML::Elements into
+download urls.
+EOM
+                my @download_urls = @_;
+
+                for my $link (@download_urls) {
+                    $link = $link->attr('href');
+                }
+
+                # Must return them, because this coderef was called by value not
+                # by reference.
+                return @download_urls;
+            };
+        }
+    };
+
+    # Call download_links_callback or call default one to strip off
+    # HTML::Element crap.
+    @download_urls = $links_callback->(@download_urls);
+    vmsg <<EOM;
+Determined download urls to be:
+[
+@{[@download_urls]}
+]
+EOM
+
+    # Return a ref to the array of download urls, because lookup()'s API only
+    # allows it to return a single value, but that single value does not have to
+    # a scalar. It can be a array ref, which is used here. This works, because
+    # what is returned here by lookup() is passed unchanged to download(), which
+    # is also part of this API, so I can use what I return here as I please
+    # inside download().
+    return \@download_urls;
+}
+
+
+=item download($temp_dir, $download_url);
+
+download() uses App::Fetchware's utility function download_http_url() to
+download all of the urls that lookup() returned. If the user specifed a
+C<user_agent> configuration option, then that option is passed along to
+download_http_url()'s call to HTTP::Tiny.
+
+=cut
+
+sub download ($;$) {
+    my ($temp_dir, $download_url) = @_;
+
+    msg 'Downloading the download urls lookup() determined.';
+
+    my @download_file_paths;
+    # Loop over @$download_url to download all user specified URLs to temp_dir.
+    for my $url (@$download_url) {
+        # Use user specified agent if they asked for it.
+        if (defined config('user_agent')) {
+            vmsg <<EOM;
+Downloadig url
+[$url]
+using the user specified user_agent
+[@{[config('user_agent')]}]
+EOM
+            my $downloaded_file =
+                download_http_url($url, agent => config('user_agent'));
+            push @download_file_paths, $downloaded_file;
+        } else {
+            vmsg "Downloading url [$url].";
+            my $downloaded_file = download_http_url($url);
+            push @download_file_paths, $downloaded_file;
+        }
+    }
+
+    ###BUGALERT### Should I use a special var to print a \n between each array
+    #elelemnt for pretty printing???
+    vmsg <<EOM;
+Downloaded specified urls to the following paths:
+[
+@{[@download_file_paths]}
+]
+EOM
+
+    # AKA $package_path.
+    return \@download_file_paths;
+}
+
+
+
+=item verify($download_url, $package_path);
+
+verify() simply calls App::Fetchware's :UTIL subroutine do_nothing(), which as
+you can tell from its name does nothing, but return. The reason for the useless
+do_nothing() call is simply for better documentation, and standardizing how to
+override a App::Fetchware API subroutine in order for it to do nothing at all,
+so that you can prevent the original App::Fetchware subroutine from doing what
+it normally does.
+
+=cut
+
+sub verify ($;$) {
+    vmsg <<EOM;
+Skipping verify subroutine, because HTMLPageSync does not need to verify anything
+EOM
+    do_nothing();
+}
+
+
+###BUGALERT### Decide if overridden subs should use original names or if they
+#should change them. I think they should change them!
+=item unarchive($package_path);
+
+unarchive() takes the $package_path, which is really an array ref of the paths
+of the files that download() copied, and copies them the the user specified
+destination directory, C<destination_directory>.
+
+=cut
+
+sub unarchive ($) {
+    # AKA $package_path.
+    my $download_file_paths = shift;
+
+    msg <<EOM;
+Copying files downloaded to a local temp directory to final destination directory.
+EOM
+
+    # Copy over the files that have been returned by download().
+    for my $file_path (@$download_file_paths) {
+        vmsg <<EOM;
+Copying [$file_path] -> [@{[config('destination_directory')]}].
+EOM
+        ###BUGALERT### Should this die and all the rest be croaks instead???
+        cp($file_path, config('destination_directory')) or die <<EOD;
+fetchware: run-time error. Fetchware failed to copy the file [$file_path] to the
+destination directory [@{[config('destination_directory')]}].
+The OS error was [$!].
+EOD
+    }
+
+    vmsg 'Successfully copied files to destination directory.';
+
+    return 'True indicating success!';
+}
+
+
+=item build($build_path);
+
+build() does the same thing as unarchive() and verify(), and that is nothing by
+calling App::Fetchware's do_nothing() subroutine to better document the fact
+that it does nothing.
+
+=cut
+
+sub build ($) {
+    vmsg <<EOM;
+Skipping build subroutine, because HTMLPageSync does not need to build anything
+EOM
+    do_nothing();
+}
+
+
+=item install();
+
+install() does nothing by calling App::Fetchware's :UTIL subroutine
+do_nothing(), which does nothing.
+
+=cut
+
+sub install (;$) {
+    vmsg <<EOM;
+Skipping install subroutine, because HTMLPageSync does not need to install anything
+EOM
+    do_nothing();
+}
+
+
+=item end();
+
+end() chdir()s back to the original directory, and cleans up the temp directory
+just like the one in App::Fetchware does. App::Fetchware::HTMLPageSync
+
+=cut
+
+
+=item uninstall($build_path);
+
+Uninstalls App::Fetchware::HTMLPageSync by recursivly deleting the
+C<destination_directory> where it stores the wallpapers or whatever you
+specified it to download for you.
+
+=over
+NOTICE: uninstalling your App::Fetchware::HTMLPageSync packagw B<will> B<delete>
+the contents of that package's associated C<destination_directory>! If you would
+like to keep your contents of your C<destination_directory> then either manually
+delete the pacakge you want to delete from your fetchware database directory, or
+just recursively copy the contents of your C<destination_directory> to a backup
+directory somewhere else.
+=back
+
+=cut
+
+sub uninstall ($) {
+    my $build_path = shift;
+
+    msg <<EOM;
+Uninstalling this HTMLPageSync package by deleting your destination directory.'
+EOM
+
+    ###BUGALERT### Before release go though all of Fetchware's API, and subifiy
+    #each main component like lookup and download were, the later ones were not
+    #done this way. That way I can put say chdir_to_build_path() here instead of
+    #basicaly copying and pasting the code like I do below. Also
+    #chdir_to_build_path() can be put in :OVERRIDE_UNINSTALL!!! Which I can use
+    #here.
+    chdir $build_path or die <<EOD;
+fetchware: Failed to uninstall the specified package and specifically to change
+working directory to [$build_path] before running make uninstall or the
+uninstall_commands provided in the package's Fetchwarefile. Os error [$!].
+EOD
+
+    if ( defined config('destination_directory')) {
+        # Use File::Path's remove_tree() to delete the destination_directory
+        # thereby "uninstalling" this package. Will throw an exception that I'll
+        # let the main eval in bin/fetchware catch, print, and exit 1.
+        vmsg <<EOM;
+Deleting entire destination directory [@{[config('destination_directory')]}].
+EOM
+        remove_tree(config('destination_directory'));
+    } else {
+        die <<EOD;
+fetchware: Failed to uninstall the specified App::Fetchware::HTMLPageSync
+package, because no destination_directory is specified in its Fetchwarefile.
+This configuration option is required and must be specified.
+EOD
+    }
+
+    return 'True for success.';
+}
+
+
+
+
+
+
+1;
---
+++ t/HTMLPageSync.t
@@ -0,0 +1,227 @@
+#!perl
+# HTMLPageSync.t tests App::Fetchware::HTMLPageSync.
+# Pretend to be bin/fetchware, so that I can test App::Fetchware as though
+# bin/fetchware was calling it.
+# This is needed in HTMLPageSync, because HTMLPageSync uses start() and end()
+# from App::Fetchware, and these subs have different behavior depending on what
+# package they are called in. In package fetchware they behave as they will be
+# called by bin/fetchware when HTMLPageSync is used in a Fetchwarefile. In other
+# packages they have the behavior of configuration subroutines that allow you to
+# specify a coderef to override their behavior. Therefore, I need to pretend to
+# be bin/fetchware to get the behavior I want.
+package fetchware;
+use strict;
+use warnings;
+use diagnostics;
+use 5.010;
+
+# Test::More version 0.98 is needed for proper subtest support.
+use Test::More 0.98 tests => '10'; #Update if this changes.
+use App::Fetchware qw(!:DEFAULT :TESTING);
+use Test::Deep;
+use Path::Class;
+use File::Spec::Functions 'updir';
+use File::Temp 'tempdir';
+use Cwd 'cwd';
+
+
+
+# Set PATH to a known good value.
+$ENV{PATH} = '/usr/local/bin:/usr/bin:/bin';
+# Delete *bad* elements from environment to make it safer as recommended by
+# perlsec.
+delete @ENV{qw(IFS CDPATH ENV BASH_ENV)};
+
+# Test if I can load the module "inside a BEGIN block so its functions are exported
+# and compile-time, and prototypes are properly honored."
+# There is no ':OVERRIDE_START' to bother importing.
+BEGIN { use_ok('App::Fetchware::HTMLPageSync', ':DEFAULT'); }
+
+# Print the subroutines that App::Fetchware::HTMLPageSync imported by default
+# when I used it.
+diag("App::Fetchware's default imports [@App::Fetchware::HTMLPageSync::EXPORT]");
+
+
+
+subtest 'test HTMLPageSync start()' => sub {
+    skip_all_unless_release_testing();
+
+    my $temp_dir = start();
+
+    ok(-e $temp_dir, 'check start() success');
+    
+    # chdir() so File::Temp can delete the tempdir.
+    chdir();
+
+};
+
+
+subtest 'test HTMLPageSync lookup()' => sub {
+    skip_all_unless_release_testing();
+
+    # Test the page that is the only reason I wrote this!
+    html_page_url $ENV{FETCHWARE_HTTP_LOOKUP_URL};
+    destination_directory 't';
+    user_agent
+        'Mozilla/5.0 (X11; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1';
+
+    my $wallpaper_urls = lookup();
+
+    cmp_deeply(
+        $wallpaper_urls,
+        eval(expected_filename_listing()),
+        'checked lookup()s return value for acceptable values.'
+    );
+
+    # Test for extracting out links.
+    html_treebuilder_callback sub {
+    my $h = shift;
+
+    #parse out archive name.
+    my $link = $h->as_text();
+    if ($link =~ /\.(tar\.(gz|bz2|xz)|(tgz|tbz2|txz))$/) {
+        # If its an archive return true indicating we should keep this
+        # HTML::Element.
+        return 'True';
+    } else {
+        return undef; # return false indicating do not keep this one.
+    }
+};
+
+    my $html_urls = lookup();
+
+    cmp_deeply(
+        $html_urls,
+        array_each(
+            re(qr/\.(tar\.(gz|bz2|xz)|(tgz|tbz2|txz))$/)
+        ),
+        'checked lookup()s return value for acceptable values again.'
+    );
+
+    download_links_callback sub {
+        my @download_urls = @_;
+
+        my @filtered_urls;
+        for my $link (@download_urls) {
+            # Strip off HTML::Element crap.
+            $link = $link->attr('href');
+            # Keep links that are absolute.
+            if ($link =~ m!^(ftp|http|file)://!) {
+                push @filtered_urls, $link;
+            }
+        }
+
+        # Return the filtered urls not the provided unfiltered @download_urls.
+        return @filtered_urls;
+    };
+
+    my $abs_urls = lookup();
+
+    cmp_deeply(
+        $abs_urls,
+        array_each(
+            re(qr!^(ftp|http|file)://!)
+        ),
+        'checked lookup()s return value for acceptable values yet again.'
+    );
+};
+
+# Created here so it can be shared with unarchive()'s subtest.
+my $download_file_paths;
+subtest 'test HTMLPageSync download()' => sub {
+    skip_all_unless_release_testing();
+
+
+    # FETCHWARE_HTTP_DOWNLOAD_URL is manually updated will break whenever a new
+    # version of Apache 2.2 comes out.
+    # download() wants an array ref.
+    $download_file_paths = download('t', [ $ENV{FETCHWARE_HTTP_DOWNLOAD_URL} ]);
+    diag("DFP");
+    diag explain $download_file_paths;
+
+    is($download_file_paths->[0],
+        file($ENV{FETCHWARE_HTTP_DOWNLOAD_URL})->basename(),
+        'checked download() success.');
+
+    #NOTE: Downloaded file is savied to the cwd(), but that value is later
+    #copied to the destination_directory 't';
+};
+
+
+subtest 'test HTMLPageSync verify()' => sub {
+    is(verify('dummy', 'args'), undef, 'checked verify() success.');
+};
+
+
+subtest 'test HTMLPageSync unarchive()' => sub {
+    skip_all_unless_release_testing();
+
+    ok(unarchive($download_file_paths),
+        'checked unarchive() success');
+
+    # unarchive() needs an array reference.
+    eval_ok(sub { unarchive([ "file-that-doesn-t-exist-$$" ])},
+        qr/fetchware: run-time error. Fetchware failed to copy the file \[/,
+        'checked unarchive exception');
+
+};
+
+
+subtest 'test HTMLPageSync build()' => sub {
+    is(verify('dummy arg'), undef, 'checked build() success.');
+};
+
+
+subtest 'test HTMLPageSync install()' => sub {
+    is(install(), undef, 'checked install() success.');
+};
+
+
+subtest 'test HTMLPageSync uninstall()' => sub {
+    skip_all_unless_release_testing();
+
+    ###BUGALERT### How do I test this without actually deleting my t testing
+    #directory that I'm using as my destination_directory???
+#    ok(uninstall(cwd()),
+#        'checked uninstall() success');
+
+    eval_ok(sub {uninstall("$$-@{[int(rand(383889))]}")},
+        qr/fetchware: Failed to uninstall the specified package and specifically/,
+        'checked uninstall() $build_path exception');
+
+    # Delete the destination_directory configuration option to test for the
+    # exception of it not existing.
+    config_delete('destination_directory');
+
+    eval_ok(sub {uninstall(cwd())},
+        <<EOE, 'checked uninstall() destination_directory exception');
+fetchware: Failed to uninstall the specified App::Fetchware::HTMLPageSync
+package, because no destination_directory is specified in its Fetchwarefile.
+This configuration option is required and must be specified.
+EOE
+
+};
+
+
+subtest 'test HTMLPageSync end()' => sub {
+    skip_all_unless_release_testing();
+
+    my $tempdir = tempdir("fetchware-$$-XXXXXXXXXX", TMPDIR => 1, CLEANUP => 1);
+    diag("td[$tempdir]");
+
+    ok(-e $tempdir, 'checked end() tempdir creationg');
+
+    end();
+
+    ok( (not -e $tempdir) , 'checked end() success');
+};
+
+
+
+###BUGALERT### Add tests for bin/fetchware's cmd_*() subroutines!!! This
+#subclasses test suite is incomplete without tests for those too!!!
+
+
+# Remove this or comment it out, and specify the number of tests, because doing
+# so is more robust than using this, but this is better than no_plan.
+#done_testing();
